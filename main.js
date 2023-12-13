ddc0baf901d2b209692a86475711615d3613b76e
sora <133684349+sorawaslocked@users.noreply.github.com>
2023-12-13 21:29:43 +0600

    Add conditional to handle invalid user input

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const CHOICES = ["Rock", "Paper", "Scissors"];
 
 alert("Submit your choice through a prompt");
 alert("1 is for rock, 2 is for paper, 3 is for scissors");
-let userChoice = prompt("What's your choice?");
+let userChoice = +prompt("What's your choice?");
+if (userChoice != 1 || userChoice != 2 || userChoice != 3) {
+  alert("Enter a valid choice!");
+}
 
 
 function getComputerChoice() {
