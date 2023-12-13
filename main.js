f258803f1f62303640d8240a5a82d455dc96b938
sora <133684349+sorawaslocked@users.noreply.github.com>
2023-12-13 21:42:45 +0600

    Add loop that prompts user if given invalid input

--- main.js
+++ main.js
@@ -3,9 +3,19 @@ const CHOICES = ["Rock", "Paper", "Scissors"];
 
 alert("Submit your choice through a prompt");
 alert("1 is for rock, 2 is for paper, 3 is for scissors");
+
 let userChoice = +prompt("What's your choice?");
-if (userChoice != 1 && userChoice != 2 && userChoice != 3)
-  console.log("Enter a valid choice!");
+let isChoiceValid = true;
+
+do {
+  if (userChoice != 1 && userChoice != 2 && userChoice != 3) {
+    userChoice = prompt("Enter a valid choice!");
+    isChoiceValid = false;
+  }
+  else {
+    isChoiceValid = true;
+  }
+} while (isChoiceValid === false);
 
 
 function getComputerChoice() {
