f57dd13fab3d703ca2bc65d059716ed61ac8695a
sora <133684349+sorawaslocked@users.noreply.github.com>
2023-12-13 21:25:07 +0600

    Add game instructions message and store user input

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const CHOICES = ["Rock", "Paper", "Scissors"];
 
+
+alert("Submit your choice through a prompt");
+alert("1 is for rock, 2 is for paper, 3 is for scissors");
+let userChoice = prompt("What's your choice?");
+
+
 function getComputerChoice() {
   let randIndex = Math.floor(Math.random() * 3);
   return CHOICES[randIndex];
