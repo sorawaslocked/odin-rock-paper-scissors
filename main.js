2a3abe073ede9c11059dc4dbfde65888b6d6926d
sora <133684349+sorawaslocked@users.noreply.github.com>
2023-12-14 10:59:06 +0600

    feat: Add getRoundWinner function

--- main.js
+++ main.js
@@ -12,23 +12,48 @@ function getComputerChoice() {
 }
 
 function validatePlayerInput(input) {
-  if (input.toLowerCase() !== CHOICES[0]
-    && input.toLowerCase() !== CHOICES[1]
-    && input.toLowerCase() !== CHOICES[2]
+  if (input !== CHOICES[0]
+    && input !== CHOICES[1]
+    && input !== CHOICES[2]
     ) return false;
   return true;
 }
 
+function getRoundWinner(playerChoice, computerChoice) {
+  switch (playerChoice) {
+    case "rock":
+      if (computerChoice === "rock")
+        return "tie";
+      else if (computerChoice === "paper")
+        return "computer";
+      return "player";
+
+    case "paper":
+      if (computerChoice === "rock")
+        return "player";
+      else if (computerChoice === "paper")
+        return "tie";
+      return "computer";
+
+    case "scissors":
+      if (computerChoice === "rock")
+        return "computer";
+      else if (computerChoice === "paper")
+        return "player";
+      return "tie";
+  }
+}
+
 function playSingleRound() {
   let computerChoice = getComputerChoice();
-  let playerChoice = prompt("What's your choice?");
+  let playerChoice = prompt("What's your choice?").toLowerCase();
   let isChoiceValid = validatePlayerInput(playerChoice);
 
   while (isChoiceValid === false) {
-    playerChoice = prompt("Enter a valid choice");
+    playerChoice = prompt("Enter a valid choice").toLowerCase();
     isChoiceValid = validatePlayerInput(playerChoice);
   }
-
+  
   let roundWinner = getRoundWinner(playerChoice, computerChoice);
   if (roundWinner === "tie") {
     alert("Tie!");
